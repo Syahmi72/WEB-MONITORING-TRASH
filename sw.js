7b4c9adf11d5bf8be3e8abae3fcaa8ea52be1802
MacBook <philipsyahmi82@gmail.com>
2026-05-04 13:16:33 +0800

    Feat: Finalisasi Landing Page 3D dan Integrasi PWA Aplikasi

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker Minimalist untuk memancing tombol Install PWA
+self.addEventListener('fetch', function(event) {
+    // Kosong tidak apa-apa, yang penting file sw.js ada dan terdaftar
+});
