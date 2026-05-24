4da4a81561bac80b346451a49a6f61b90f671df2
MacBook <philipsyahmi82@gmail.com>
2026-05-24 18:22:26 +0800

    Update HAKI: Tambah fitur PWA Aplikasi & Tema Liquid 3D

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
-// Service Worker Minimalist untuk memancing tombol Install PWA
-self.addEventListener('fetch', function(event) {
-    // Kosong tidak apa-apa, yang penting file sw.js ada dan terdaftar
+const CACHE_NAME = 'elektron-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './login.html',
+  './icon.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch API (Bypass Firebase agar sensor tetap Real-time)
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('firebase') || event.request.url.includes('google')) {
+    return; // Jangan cache data sensor, biarkan live!
+  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
